6d5d6df628de9ed8632aaf4245dd044be1202f7a
Lyle <86522347+cgglyle@users.noreply.github.com>
2022-09-18 18:35:57 +0800

    refactor: 移动线程池至测试类

--- boson-graviton-spring-boot-test/src/main/java/com/cgglyle/boson/graviton/test/config/ThreadPoolExecutorConfig.java
+++ boson-graviton-spring-boot-test/src/main/java/com/cgglyle/boson/graviton/test/config/ThreadPoolExecutorConfig.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cgglyle.boson.graviton.config;
+package com.cgglyle.boson.graviton.test.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,22 +33,34 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 @Configuration
 public class ThreadPoolExecutorConfig {
-    private static final int CORE_POOL_SIZE = 10;
-    private static final int MAX_POOL_SIZE = 20;
-    private static final int QUEUE_CAPACITY = 1000;
-    private static final String NAME_PREFIX = "Graviton-Log ";
+    /**
+     * 核心线程数
+     */
+    private int corePoolSize = 10;
+    /**
+     * 最大线程数
+     */
+    private int maxPoolSize = 20;
+    /**
+     * 队列大小
+     */
+    private int queueCapacity = 1000;
+    /**
+     * 线程名前缀
+     */
+    private String namePrefix = "Graviton-Log ";
 
-    @Bean(name = "gravitonLogThreadPool")
+    @Bean(name = "gravitonLogPool")
     public Executor asyncServiceExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         //配置核心线程数
-        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setCorePoolSize(corePoolSize);
         //配置最大线程数
-        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setMaxPoolSize(maxPoolSize);
         //配置队列大小
-        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setQueueCapacity(queueCapacity);
         //配置线程池中的线程的名称前缀
-        executor.setThreadNamePrefix(NAME_PREFIX);
+        executor.setThreadNamePrefix(namePrefix);
         // rejection-policy：当pool已经达到max size的时候，如何处理新任务
         // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
